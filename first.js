6b51662e8709851ce56ad16cfd3030bdfba94d3c
pemmanitejasri <venakateshpemmani65@gmail.com>
2025-09-06 12:36:40 +0530

    hi

--- first.js
+++ first.js
@@ -1,1 +1,28 @@
 console.log("hello world,tejasri")
+
+let name="Dev Astra"
+console.log(name)
+
+let num=10
+console.log(num)
+
+let x;
+console.log(x)
+
+let y=null
+
+console.log(y)
+
+
+console.log(typeof name)
+console.log(typeof num)
+console.log(typeof x)
+
+let person={
+    name:"teja",
+    age:18
+}
+
+console.log(person.name)
+console.log(person.age)
+console.log(typeof person)
